f0e88d333aca140b26c4d303caaa47923d7ef082
Andrés Ponce <andres.ponce@epn.edu.ec>
2023-10-06 03:06:16 -0500

    feat(shared): add header-footer-template

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
-
-
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HeaderFooterTemplate } from './templates/header-footer-template/header-footer-template.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    FooterComponent,
+    HeaderComponent,
+    HeaderFooterTemplate
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    HeaderFooterTemplate
   ]
 })
 export class SharedModule { }
